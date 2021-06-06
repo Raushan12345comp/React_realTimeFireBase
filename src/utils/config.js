16c288da235b5884dccf9b9ac64b2c8b9df49a4f
Raushan12345comp <60733038+Raushan12345comp@users.noreply.github.com>
2021-06-06 15:35:24 +0530

    first-commit

---
+++ src/utils/config.js
@@ -0,0 +1,21 @@
+//TODO: DONE: add firebase configuration and export it
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB1xcrbuyFml0MB1HqbW9lGVrtDsa5tIfc",
+  authDomain: "mygitapp5.firebaseapp.com",
+  databaseURL: "https://mygitapp5-default-rtdb.firebaseio.com",
+  projectId: "mygitapp5",
+  storageBucket: "mygitapp5.appspot.com",
+  messagingSenderId: "531406972957",
+  appId: "1:531406972957:web:ad7872e98561e85f38bef4",
+  measurementId: "G-G61SJDQXPZ"
+};
+
+//image configuration
+export const imageConfig = {
+    quality: 0.2,
+    maxWidth: 800,
+    maxHeight: 600,
+    autoRotate: true
+  };
+  
